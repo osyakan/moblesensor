8899b73331b8c117df15613ab83ac6322ff8d394
kan kusakabe <kusakabe.kan.v5@elms.hokudai.ac.jp>
2023-11-08 17:53:36 +0900

    enable to select sensors
    - センサの種類を選択可能に
    - メモリリークを避けるために途中でセンサ値をリセットする
    - 周期は120hz(確か)

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -25,23 +25,27 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import android.graphics.Color;
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import java.text.SimpleDateFormat
 
 
 class MainActivity : AppCompatActivity(), SensorEventListener{
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
     private lateinit var sensorManager: SensorManager
-    private lateinit var textInfo: TextView
-    private lateinit var textView: TextView
     private lateinit var mChart: LineChart
-    private val labels: Array<String> = arrayOf("accX","accY","accZ","Pressure")
+    private lateinit var sensorSpinner: Spinner
+    private var sensorMap = mutableMapOf<String, Sensor>()
     private val colors = arrayOf(Color.BLUE,Color.RED,Color.GREEN,Color.CYAN)
     private var sensors: Array<Float> = arrayOf(0f,0f,0f,0f)
-
-//    private lateinit var accSensor: Sensor;
-//    private lateinit var pressureSensor: Sensor;
+    private lateinit var selectedSensor: Sensor
+    val maxEntries = 50f // max sensor keep size
+    val samplingHz = 120 // サンプリング周波数
+    val sensorInterval = (1000000/samplingHz).toInt()
+    private val labels: Array<String> = arrayOf("X","Y","Z","W")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,32 +53,63 @@ class MainActivity : AppCompatActivity(), SensorEventListener{
 
         // get SensorManager
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        textInfo = findViewById(R.id.text_info)
-        textView = findViewById(R.id.text_view)
-
+        // set default sensor type
+        selectedSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        // get Spinner
+        sensorSpinner = findViewById(R.id.sensorlist)
         // get LineChart
         mChart = findViewById(R.id.chart)
         mChart.data = LineData()
         mChart.description.isEnabled = true
         mChart.axisRight.isEnabled = false
 
+        // set available sensors
+        val sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL)
+        for (sensor in sensorList){
+            sensorMap[sensor.name] = sensor
+        }
+        Log.d("sensors","sensor values: ${sensorMap.values}")
+
+        // set Spinner
+        val sensorNameList = ArrayList(sensorMap.keys)
+        val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,sensorNameList)
+        sensorSpinner.adapter = adapter
+        // set event
+        sensorSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedSensorName = sensorNameList[position]
+                var NewSelectedSensor = sensorMap[selectedSensorName]
+
+                if(NewSelectedSensor==null){ //set acc if selected sensor is none
+                    NewSelectedSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+                }
+                if (selectedSensor!=NewSelectedSensor && NewSelectedSensor!=null){
+                    sensorManager.unregisterListener(this@MainActivity) // unregister old sensor
+                    selectedSensor = NewSelectedSensor
+                    // set new sensor
+                    sensorManager.registerListener(this@MainActivity,selectedSensor,sensorInterval)
+                }
+                initializeChart()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+
     }
 
     override fun onResume() {
         super.onResume()
-        // Register Listener
-        //読み込みたいセンサをここで登録する
-        // スマホに搭載されていないセンサの値は読まない（エラーにはならない）
-        // 参照URL: https://developer.android.com/guide/topics/sensors?hl=ja
-        sensorManager.registerListener(this,
-            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),// accelerometer
-            SensorManager.SENSOR_DELAY_NORMAL)
-        sensorManager.registerListener(this,
-            sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE), // barometric pressure
-            SensorManager.SENSOR_DELAY_NORMAL)
+        // Re-register Listener
+        sensorManager.registerListener(this, selectedSensor, SensorManager.SENSOR_DELAY_NORMAL)
 
+        initializeChart()// initialize chart
 
-        //initialize chart
+    }
+
+    //initialize chart
+    private fun initializeChart(){
         var linedatasets = mutableListOf<ILineDataSet>()
         for (i in 0..3){
             var linedataset: LineDataSet = LineDataSet(null,labels[i])
@@ -84,48 +119,58 @@ class MainActivity : AppCompatActivity(), SensorEventListener{
             linedatasets.add(linedataset)
         }
         mChart.data = LineData(linedatasets)
+        mChart.invalidate()
     }
 
     override fun onPause() {
         super.onPause()
+        initializeChart()
         // Free Listener
         sensorManager.unregisterListener(this)
+
     }
 
-    public override fun onSensorChanged(event: SensorEvent?){
-        // accelerometers' value are changed
-        if (event?.sensor != null){
-//            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER){ // 圧力センサの処理
-//                // get sensor values
-//                for (i in 0..2){sensors[i] = event.values[i]}
-//            }
-            if(event.sensor.type == Sensor.TYPE_PRESSURE){ // 気圧センサの処理 => なぜか表示されない
-                sensors[3] = event.values[0]
-                for (i in 0..3){sensors[i] = event.values[0]}
-            }
+    public override fun onSensorChanged(event: SensorEvent){
+        if (event.sensor!=selectedSensor)
+            return
+        val sensors = event.values
 
-            // show value string
-            var str : String = "センサの値\n"
-            for (i in 0..3){str+="${labels[i]}: ${sensors[i]}\n"}
-            textView.setText(str)
+        // figure out
+        var data: LineData = mChart.data
+        var initFlag = false
+        if (data!=null){
+            for (i in 0 until sensors.size){ // in each sensor value
+                var iLineDataSet: ILineDataSet = data.getDataSetByIndex(i)
 
-            // figure out
-            var data: LineData = mChart.data
-//            textView.append("${data.dataSetCount}")
-            if (data!=null){ //  && data.dataSetCount>0
-                for (i in 0..3){ // in each sensor value
-                    var iLineDataSet: ILineDataSet = data.getDataSetByIndex(i)
-                    data.addEntry(Entry(iLineDataSet.entryCount.toFloat(), sensors[i]),i)
-                    data.notifyDataChanged()// it must be called when data is updated
+                // keep sensor value length for prevent memory leak
+//                Log.d("memory","${iLineDataSet.entryCount}:${iLineDataSet}")
+                if (iLineDataSet.entryCount>=maxEntries*50){
+                    initFlag = true
                 }
 
-                mChart.notifyDataSetChanged()// it must be called when data is updated
-                mChart.setVisibleXRangeMaximum(50F)// point width for x-axis in the window
-                mChart.moveViewToX(data.entryCount.toFloat()-50F)// set auto scroll to latest value
-
+                data.addEntry(Entry(iLineDataSet.entryCount.toFloat(), sensors[i]),i)
+                data.notifyDataChanged()// it must be called when data is updated
             }
 
+            // fit y-axis range
+            val maxis = mChart.axisLeft
+            maxis.axisMaximum = data.getYMax() + 1f
+            maxis.axisMinimum = data.getYMin() - 1f
+
+
+            mChart.notifyDataSetChanged()// it must be called when data is updated
+            mChart.setVisibleXRangeMaximum(maxEntries)// point width for x-axis in the window
+            mChart.moveViewToX(data.entryCount.toFloat()-maxEntries)// set auto scroll to latest value
+
+
+            // prevent memory leak
+            if (initFlag){
+                initializeChart()
+            }
         }
+
+
+
     }
 
     public override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
